225c003ec7bf897e53ac304695574f94ef940aaa
John Nezzi <johnnezzi@yahoo.com>
2021-08-08 21:38:07 +0100

    create class validator

--- src/controllers/encode.controller.ts
+++ src/controllers/encode.controller.ts
@@ -1,16 +1,18 @@
 import {
-  Body, Controller,
+  Body,
+  Controller,
   Post,
+  UsePipes,
 } from '@nestjs/common';
 import { UrlDto } from '../dtos/Url.dto';
 import { EncodeService } from '../services/encode.service';
-
+import { ValidationPipe } from '../shared/validation.pipe';
 @Controller()
 export class EncodeController {
   constructor(private encodeService: EncodeService) {}
 
   @Post('shrinkme/encode')
-
+  @UsePipes(new ValidationPipe())
   async encode(@Body() urlDto: UrlDto) {
     const url = await this.encodeService.processUrls(urlDto);
 
