f76dafb3105e6218c67328ff7637cf5bcbef7b20
John Nezzi <johnnezzi@yahoo.com>
2021-08-08 20:41:13 +0100

    write second test

--- src/services/encode.service.spec.ts
+++ src/services/encode.service.spec.ts
@@ -22,5 +22,15 @@ describe('EncodeService', () => {
         url: 'http://localhost:3000/a2dw9ijp',
       });
     });
+
+    it('if a url doesnt exist it calls the encode function', async () => {
+      encodeService.checkIfExists = jest.fn(() => null);
+      encodeService.encode = jest.fn(() => 'a2dw9ijp');
+      expect(encodeService.encode).not.toHaveBeenCalled();
+      await encodeService.processUrls({
+        url: 'http://test.com',
+      });
+      expect(encodeService.encode).toHaveBeenCalled();
+    });
   });
 });
