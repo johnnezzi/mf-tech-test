359fdaa5b7d1567d677a653a68e96ffb7fb8b03b
John Nezzi <johnnezzi@yahoo.com>
2021-08-08 20:35:22 +0100

    create check if exists function and tweek process urls

--- src/services/encode.service.ts
+++ src/services/encode.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { UrlDto } from '../../../nestjs copy/src/dtos/url.dto';
+
+const obj = {};
+const baseUrl = 'http://localhost:3000/';
 
 @Injectable()
-export class EncodeService {}
+export class EncodeService {
+  async processUrls(urlDto: UrlDto): Promise<{ url: string }> {
+    const code = this.checkIfExists(urlDto.url);
+    // if not encodes a url
+    // returns a url in json object
+    return {
+      url: baseUrl + code,
+    };
+  }
+
+  checkIfExists(url: string): any {
+    return url;
+  }
+}
